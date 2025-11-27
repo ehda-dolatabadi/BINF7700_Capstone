4b325ab39bb4b0f3370e3b324a9aa1b32172543f
Ehda <ehda.dolatabadi@gmail.com>
2025-11-26 21:15:18 -0500

    update

--- scripts/seurat/Rscripts/10_score_markers.R
+++ scripts/seurat/Rscripts/10_score_markers.R
@@ -51,7 +51,7 @@ dev.off()
 
 # Plot individual markers
 for (marker in markers_available){
-  png(file.path(outdir, paste0(id, marker, "_timepoints_violin_score.png")), width = 1600, height = 1200, res = 150)
+  png(file.path(outdir, paste0(id, "_", marker, "_timepoints_violin_score.png")), width = 1600, height = 1200, res = 150)
   print(VlnPlot(obj, features = marker, group.by = "orig.ident", pt.size = 0) +
     geom_hline(yintercept = 0, linetype = "dashed", color = "red") +
     labs(x = NULL))
@@ -66,7 +66,7 @@ if ("seurat_clusters" %in% colnames(obj@meta.data)) {
   dev.off()
 
   for (marker in markers_available){
-    png(file.path(outdir, paste0(id, marker, "_clusters_violin_score.png")), width = 1600, height = 1200, res = 150)
+    png(file.path(outdir, paste0(id, "_", marker, "_clusters_violin_score.png")), width = 1600, height = 1200, res = 150)
     print(VlnPlot(obj, features = marker, group.by = "seurat_clusters", pt.size = 0) +
       geom_hline(yintercept = 0, linetype = "dashed", color = "red") +
       labs(x = NULL))
